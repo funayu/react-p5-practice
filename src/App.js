ad1c17adb2fcd1c17cb4e6bdefba184725720de5
funayu <funayu0908@gmail.com>
2021-07-25 17:17:51 +0900

    四角を書いた

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from "react"
+import P5Wrapper from "react-p5-wrapper"
+import testSketch from "./testSketch"
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => (
+  <P5Wrapper sketch={testSketch} />
+)
 
 export default App;
---
+++ src/testSketch.js
@@ -0,0 +1,28 @@
+const testSketch = (p) => {
+  let rotation = 0
+
+  // 初期設定
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL)
+  }
+
+  // P5Wrapperコンポーネントからのpropsの受け渡し
+  p.myCustomRedrawAccordingToNewPropsHandler = (props) => {
+    if (props.rotation) {
+      rotation = props.rotation * Math.PI / 180
+    }
+  }
+
+  // 各フレームごとの描画内容
+  p.draw = () => {
+    p.background(100)
+    p.normalMaterial()
+    p.noStroke()
+    p.push()
+    p.rotateY(rotation)
+    p.box(100)
+    p.pop()
+  }
+}
+
+export default testSketch
